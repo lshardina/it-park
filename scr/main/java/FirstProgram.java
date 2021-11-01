75c5c7cf6e047684a8053e74bd4ac030166e846f
lshardina <lshardina@bfrcom.com>
2021-11-01 16:58:55 +0500

    add first program

---
+++ scr/main/java/FirstProgram.java
@@ -0,0 +1,6 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Привет, Миррр!!!");
+        System.out.println("ох, это было  нелегко...");
+    }
+}
