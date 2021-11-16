b016b42a1e5d2975c535324d67f0269fe6fe92f7
lshardina <lshardina@bfrcom.com>
2021-11-16 20:44:50 +0500

    add uslovie neotr

--- scr/main/java/Lesson4/KvadrKoren.java
+++ scr/main/java/Lesson4/KvadrKoren.java
@@ -7,16 +7,20 @@ public class KvadrKoren {
     // Вычисление квадратного корня из заданного числа
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Пожалуйста, введите число:");
-        int b = scanner.nextInt(); // Число, из которого нужно извлечь квадратный корень
-        int a = 0;
-        while (a * a < b) {
-            a++;
-        }
-        if (a * a == b) {
-            System.out.println("√" + b + " = " + a);
+        System.out.println("Пожалуйста, введите неотрицательное число:");
+        int b = scanner.nextInt();
+        if (b < 0) {
+            System.out.println("Из отрицательного числа нельзя извлекать квадратный корень");
         } else {
-            System.out.println("К сожалению, из числа " + b + " нельзя извлечь целый квадратный корень");
+            int a = 0;
+            while (a * a < b) {
+                a++;
+            }
+            if (a * a == b) {
+                System.out.println("√" + b + " = " + a);
+            } else {
+                System.out.println("К сожалению, из числа " + b + " нельзя извлечь целый квадратный корень");
+            }
         }
     }
 }
